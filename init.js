7b904bd6b94c8fa0e976d6be9aa700c28a70d10a
= <lukehoogenboom@gmail.com>
2024-03-18 19:33:31 +0100

    made background iamge default sourceand if none is set go to a color

--- init.js
+++ init.js
@@ -1,7 +1,10 @@
+let image = String(document.body.style.backgroundImage);
+image = image..substring(3, image.length - 1);
+
 class PichaiUX {
     construct(options) {
         options = {
-            source: '#008dcd',
+            source:  ?? '#008dcd',
         }
     }
 }
