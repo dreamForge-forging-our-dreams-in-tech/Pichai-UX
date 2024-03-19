098a78f783ecbac97e53e4002eae299ad9c223c2
= <lukehoogenboom@gmail.com>
2024-03-19 11:51:30 +0100

    testing for proper var accessing

--- AI/palleteMaker.js
+++ AI/palleteMaker.js
@@ -1,1 +1,5 @@
 import ColorThief from './node_modules/colorthief/dist/color-thief.mjs';
+
+import PichaiUX from '../init.js';
+alert(PichaiUX.options.source);
+
--- init.js
+++ init.js
@@ -25,3 +25,5 @@ class PichaiUX {
         }
     }
 }
+
+export { PichaiUX };
