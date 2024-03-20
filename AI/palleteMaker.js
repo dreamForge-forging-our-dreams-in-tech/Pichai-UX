00a2885bfdf8a0f4addcba077801807ecaa53713
= <lukehoogenboom@gmail.com>
2024-03-20 19:32:04 +0100

    fucking failing amend broke code completly should be solved now

--- AI/palleteMaker.js
+++ AI/palleteMaker.js
@@ -2,9 +2,9 @@ import ColorThief from './colorThief.js';
 
 import { PichaiUX } from '../init.js';
 
-function getPallete(options) {
+function getPallete (options) {
     return new Promise((resolve) => {
-        if (String(options.source).includes('#')) {
+    if(String(options.source).includes('#')) {
 
     } else {
         const colorThief = new ColorThief();
@@ -17,15 +17,20 @@ function getPallete(options) {
         img.crossOrigin = 'Anonymous';
         img.src = options.source;
     }
+    });
+}
+
+async function generate3ColorPallete(options) {
+    let colors = await getPallete(options);
 
     const root = document.documentElement;
-    let primary = colors[0].replaceAll('[', '(').replaceAll(']', ')');
-    let secondairy = colors[4].replaceAll('[', '(').replaceAll(']', ')');
-    let tertiary = colors[9].replaceAll('[', '(').replaceAll(']', ')');
+    let primary = colors[0].replaceAll('[','(').replaceAll(']',')');
+    let secondairy = colors[4].replaceAll('[','(').replaceAll(']',')');
+    let tertiary = colors[9].replaceAll('[','(').replaceAll(']',')');
 
     root.style.setProperty('--primary', `rgb ${primary}`);
     root.style.setProperty('--secondairy', `rgb ${secondairy}`);
     root.style.setProperty('--tertiary', `rgb ${tertiary}`);
 }
 
-export { generate3ColorPallete };
+    export { generate3ColorPallete };
