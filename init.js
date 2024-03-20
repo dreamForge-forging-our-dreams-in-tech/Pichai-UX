8f0c73bd0e95fb7cfcf1daa48d18a1f162700d68
= <lukehoogenboom@gmail.com>
2024-03-20 14:08:34 +0100

    removed auto selecting init options

--- init.js
+++ init.js
@@ -1,15 +1,7 @@
 import { generate3ColorPallete } from "./AI/palleteMaker.js";
 
-alert(document.body.style.backgroundImage)
-let image = String(document.body.style.backgroundImage);
-image = image.substring(3, image.length - 1);
-
 class PichaiUX {
-    constructor(options = {
-        source: image || '#008dcd',
-        darkMode: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches,
-        overrideColorsOnScroll: true
-    }) {
+    constructor(options) {
         this.options = options;
     }
 
