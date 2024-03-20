eaa307320fdc0d7d91511a681a17cddb2408c866
= <lukehoogenboom@gmail.com>
2024-03-20 13:53:24 +0100

    test7

--- init.js
+++ init.js
@@ -4,12 +4,8 @@ let image = String(document.body.style.backgroundImage);
 image = image.substring(3, image.length - 1);
 
 class PichaiUX {
-    construct(options) {
-        this.options = {
-            source: image ?? '#008dcd',
-            darkMode: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches,
-            overrideColorsOnScroll: true
-        }
+    construct(options = {source: image ?? '#008dcd', darkMode: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches, overrideColorsOnScroll: true }) {
+        this.options = options;
     }
 
     initialize() {
