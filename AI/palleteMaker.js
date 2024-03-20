af58b5a8b8314c55817f1024b0db568f4044f983
= <lukehoogenboom@gmail.com>
2024-03-20 19:19:27 +0100

    forgot to rename colors in getPallete function to resolve

--- AI/palleteMaker.js
+++ AI/palleteMaker.js
@@ -2,32 +2,35 @@ import ColorThief from './colorThief.js';
 
 import { PichaiUX } from '../init.js';
 
-let colors;
+function getPallete(options) {
+    return new Promise((resolve) => {
+        if (String(options.source).includes('#')) {
+
+        } else {
+            const colorThief = new ColorThief();
+            const img = new Image();
+
+            img.addEventListener('load', () => {
+                resolve(colorThief.getPalette(img));
+            });
+
+            img.crossOrigin = 'Anonymous';
+            img.src = options.source;
+        }
+    });
+}
 
 async function generate3ColorPallete(options) {
-    if(String(options.source).includes('#')) {
-
-    } else {
-        const colorThief = new ColorThief();
-        const img = new Image();
-    
-        img.addEventListener('load', () => {
-            colors = await colorThief.getPalette(img);
-            console.log(colors);
-        });
-    
-        img.crossOrigin = 'Anonymous';
-        img.src = options.source;
-    }
+    let colors = await getPallete(options);
 
     const root = document.documentElement;
-    let primary = colors[0].replaceAll('[','(').replaceAll(']',')');
-    let secondairy = colors[4].replaceAll('[','(').replaceAll(']',')');
-    let tertiary = colors[9].replaceAll('[','(').replaceAll(']',')');
+    let primary = colors[0].replaceAll('[', '(').replaceAll(']', ')');
+    let secondairy = colors[4].replaceAll('[', '(').replaceAll(']', ')');
+    let tertiary = colors[9].replaceAll('[', '(').replaceAll(']', ')');
 
     root.style.setProperty('--primary', `rgb ${primary}`);
     root.style.setProperty('--secondairy', `rgb ${secondairy}`);
     root.style.setProperty('--tertiary', `rgb ${tertiary}`);
 }
 
-    export { generate3ColorPallete };
+export { generate3ColorPallete };
