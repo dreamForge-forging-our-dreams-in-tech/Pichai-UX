162b0c7a07535b24fb1d966666a147707ed1295e
= <lukehoogenboom@gmail.com>
2024-03-20 13:56:56 +0100

    ttestt8

--- init.js
+++ init.js
@@ -4,27 +4,31 @@ let image = String(document.body.style.backgroundImage);
 image = image.substring(3, image.length - 1);
 
 class PichaiUX {
-    construct(options = {source: image ?? '#008dcd', darkMode: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches, overrideColorsOnScroll: true }) {
+    constructor(options = {
+        source: image || '#008dcd',
+        darkMode: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches,
+        overrideColorsOnScroll: true
+    }) {
         this.options = options;
     }
 
     initialize() {
-        let cssId = 'PichaiUXCss';  // you could encode the css path itself to generate id..
+        let cssId = 'PichaiUXCss';
         if (!document.getElementById(cssId)) {
             let head = document.getElementsByTagName('head')[0];
             let link = document.createElement('link');
             link.id = cssId;
             link.rel = 'stylesheet';
             link.type = 'text/css';
-            link.href = typeof exports != "undefined" ? 'Pichai-UX/CSS/main.css' : 'https://lukeplays33.github.io/Pichai-UX/CSS/main.css';
+            link.href = typeof exports !== 'undefined' ? 'Pichai-UX/CSS/main.css' : 'https://lukeplays33.github.io/Pichai-UX/CSS/main.css';
             link.media = 'all';
 
             head.appendChild(link);
         }
 
-        console.log(this.options)
-        generate3ColorPallete();
+        console.log(this.options);
+        // Call your other functions here (e.g., generate3ColorPallete())
     }
 }
 
-export { PichaiUX };
+export { PichaiUX }
