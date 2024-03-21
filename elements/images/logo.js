4487b2ecac3f417f19a938d70ad4dbd81403a965
= <lukehoogenboom@gmail.com>
2024-03-21 12:10:21 +0100

    test2

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -67,6 +67,7 @@ class Logo extends HTMLElement {
         this.style.backgroundImage = this.src ?? `url("${faviconUrl}")`;
 
         if (!this.hasAttribute('dynamic') || this.getAttribute('dynamic') == 'true') {
+            alert()
             this.style.backgroundImage = await generateDynamicIcon(this.style.backgroundImage);
         }
     }
