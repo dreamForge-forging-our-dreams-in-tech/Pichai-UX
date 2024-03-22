653f4f4f6ef39c24ea90bb5abe0b946aca6effde
= <lukehoogenboom@gmail.com>
2024-03-22 07:55:33 +0100

    added x-title element

---
+++ elements/text/title.js
@@ -0,0 +1,35 @@
+// Create a class for the element
+class title extends HTMLElement {
+    static observedAttributes = ["title"];
+
+    constructor() {
+        // Always call super first in constructor
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = document.title;
+
+        if(!this.hasAttribute('title') || !this.getAttribute('title') == '') {
+            this.innerHTML = this.getAttribute('title')
+        }
+    }
+
+    disconnectedCallback() {
+        console.log("Custom element removed from page.");
+    }
+
+    adoptedCallback() {
+        console.log("Custom element moved to new page.");
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.innerHTML = document.title;
+
+        if(!this.hasAttribute('title') || !this.getAttribute('title') == '') {
+            this.innerHTML = this.getAttribute('title')
+        }
+    }
+}
+
+customElements.define("x-title", title);
