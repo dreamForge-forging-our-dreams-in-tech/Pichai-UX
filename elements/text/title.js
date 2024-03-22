ff166b5922cf00ceedb573c5c97c720d8a34cc01
= <lukehoogenboom@gmail.com>
2024-03-22 09:50:01 +0100

    not statements test with text.js

--- elements/text/title.js
+++ elements/text/title.js
@@ -10,7 +10,7 @@ class title extends HTMLElement {
     connectedCallback() {
         this.innerHTML = document.title;
 
-        if(!this.hasAttribute('title') || !this.getAttribute('title') == '') {
+        if(!this.hasAttribute('title') || this.getAttribute('title') !== '') {
             this.innerHTML = this.getAttribute('title')
         }
     }
@@ -26,7 +26,7 @@ class title extends HTMLElement {
     attributeChangedCallback(name, oldValue, newValue) {
         this.innerHTML = document.title;
 
-        if(!this.hasAttribute('title') || !this.getAttribute('title') == '') {
+        if(!this.hasAttribute('title') || !this.getAttribute('title') !== '') {
             this.innerHTML = this.getAttribute('title')
         }
     }
