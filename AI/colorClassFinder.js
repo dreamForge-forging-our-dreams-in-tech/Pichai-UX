eb219027df2d786ee9166d7a6e88386896cf9c4e
= <lukehoogenboom@gmail.com>
2024-03-24 18:55:39 +0100

    test

--- AI/colorClassFinder.js
+++ AI/colorClassFinder.js
@@ -1,60 +1,52 @@
-import './Brain.js';
+import brain from 'brain.js';
 
 var net = new brain.NeuralNetwork();
 
-net.train([{
-    input: {
-      r: 0.03,
-      g: 0.7,
-      b: 0.5
-    },
-    output: {
-      black: 1
-    }
-  },
-  {
-    input: {
-      r: 0.16,
-      g: 0.09,
-      b: 0.2
-    },
-    output: {
-      white: 1
-    }
-  },
-  {
-    input: {
-      r: 0.5,
-      g: 0.5,
-      b: 1.0
-    },
-    output: {
-      white: 1
-    }
-  }
-  ]);
+// Normalize the input data
+const normalize = (value) => value / 255;
 
-function findColorClass (color) {
-    let i;
-    let r = color[0];
-    let g = color[1];
-    let b = color[2];
+net.train([
+    {
+        input: {
+            r: normalize(0.255),
+            g: normalize(0.255),
+            b: normalize(0.255)
+        },
+        output: {
+            white: 1
+        }
+    },
+    {
+        input: {
+            r: normalize(0.240),
+            g: normalize(0.240),
+            b: normalize(0.240)
+        },
+        output: {
+            white: 1
+        }
+    }
+]);
+
+function findColorClass(color) {
+    let r = normalize(color[0]);
+    let g = normalize(color[1]);
+    let b = normalize(color[2]);
 
     let result = net.run({
         r: r,
         g: g,
         b: b
-      });
+    });
 
-      console.log(result)
+    console.log(result);
 
-      for(i of Object.keys(result)) {
-        if(result[i] == Math.max(... Object.values(result))) {
-            alert(i)
+    for (let i of Object.keys(result)) {
+        if (result[i] === Math.max(...Object.values(result))) {
+            alert(i);
             return i;
-            break;
         }
-      }
+    }
 }
 
 export { findColorClass };
