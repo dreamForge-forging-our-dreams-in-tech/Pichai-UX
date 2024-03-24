d17154f6f3cf2c0895e806117ee522dcc51ea18f
= <lukehoogenboom@gmail.com>
2024-03-24 18:51:14 +0100

    test

--- AI/colorClassFinder.js
+++ AI/colorClassFinder.js
@@ -4,9 +4,19 @@ var net = new brain.NeuralNetwork();
 
 net.train([{
     input: {
-      r: 0.255,
-      g: 0.255,
-      b: 0.255
+      r: 0.03,
+      g: 0.7,
+      b: 0.5
+    },
+    output: {
+      black: 1
+    }
+  },
+  {
+    input: {
+      r: 0.16,
+      g: 0.09,
+      b: 0.2
     },
     output: {
       white: 1
@@ -14,15 +24,15 @@ net.train([{
   },
   {
     input: {
-      r: 0.240,
-      g: 0.240,
-      b: 0.240
+      r: 0.5,
+      g: 0.5,
+      b: 1.0
     },
     output: {
       white: 1
     }
   }
-]);
+  ]);
 
 function findColorClass (color) {
     let i;
