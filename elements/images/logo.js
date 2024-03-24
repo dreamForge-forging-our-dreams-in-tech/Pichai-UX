7652f0e7f009324d35811efb8d0dc99e56f085f3
= <lukehoogenboom@gmail.com>
2024-03-24 17:18:27 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -35,9 +35,9 @@ function generateDynamicIcon(image) {
                 const green = imageData.data[i + 1];
                 const blue = imageData.data[i + 2];
 
-                // Check if the pixel is not black or white
+                // Check if the pixel is not white
                 if (
-                    (red < colorTolerance || green < colorTolerance || blue < colorTolerance) && (red == rgb[0] || green == rgb[1] || blue == rgb[2])
+                    (red < colorTolerance || green < colorTolerance || blue < colorTolerance) && !(red == rgb[0] || green == rgb[1] || blue == rgb[2])
                 ) {
                     // Replace with your desired color (e.g., green)
                     imageData.data[i] = 255;//rgb[0]; // Red channel
