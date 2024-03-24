6e83fd1be6f45745b8960f70243fc5a768d6d5fe
= <lukehoogenboom@gmail.com>
2024-03-24 18:33:22 +0100

    forgot to mnake the text color finder actual use the theme color and started wortking on the color class finder api

---
+++ AI/colorClassFinder.js
@@ -0,0 +1,54 @@
+import './Brain.js';
+
+var net = new brain.NeuralNetwork();
+
+net.train([{
+    input: {
+      r: 0.03,
+      g: 0.7,
+      b: 0.5
+    },
+    output: {
+      black: 1
+    }
+  },
+  {
+    input: {
+      r: 0.16,
+      g: 0.09,
+      b: 0.2
+    },
+    output: {
+      white: 1
+    }
+  },
+  {
+    input: {
+      r: 0.5,
+      g: 0.5,
+      b: 1.0
+    },
+    output: {
+      white: 1
+    }
+  }
+]);
+
+function findColorClass (color) {
+    let r = color[0];
+    let g = color[1];
+    let b = color[2];
+    
+    let result = net.run({
+        r: r,
+        g: g,
+        b: b
+      });
+
+      for(i of Object.keys(result)) {
+        if(result[i] == Math.max(... Object.values(result))) {
+            return i;
+            break;
+        }
+      }
+}
--- AI/textColorFInder.js
+++ AI/textColorFInder.js
@@ -3,51 +3,55 @@ import './Brain.js';
 var net = new brain.NeuralNetwork();
 
 net.train([{
-    input: {
-      r: 0.03,
-      g: 0.7,
-      b: 0.5
-    },
-    output: {
-      black: 1
-    }
+  input: {
+    r: 0.03,
+    g: 0.7,
+    b: 0.5
   },
-  {
-    input: {
-      r: 0.16,
-      g: 0.09,
-      b: 0.2
-    },
-    output: {
-      white: 1
-    }
-  },
-  {
-    input: {
-      r: 0.5,
-      g: 0.5,
-      b: 1.0
-    },
-    output: {
-      white: 1
-    }
+  output: {
+    black: 1
   }
+},
+{
+  input: {
+    r: 0.16,
+    g: 0.09,
+    b: 0.2
+  },
+  output: {
+    white: 1
+  }
+},
+{
+  input: {
+    r: 0.5,
+    g: 0.5,
+    b: 1.0
+  },
+  output: {
+    white: 1
+  }
+}
 ]);
 
 function getTextColor(color) {
-    let i;
-    let result = net.run({
-        r: 1,
-        g: 0.4,
-        b: 0
-      });
+  let r = color[0];
+  let g = color[1];
+  let b = color[2];
 
-      for(i of Object.keys(result)) {
-        if(result[i] == Math.max(... Object.values(result))) {
-            return i;
-            break;
-        }
-      }
+  let i;
+  let result = net.run({
+    r: r,
+    g: g,
+    b: b
+  });
+
+  for (i of Object.keys(result)) {
+    if (result[i] == Math.max(...Object.values(result))) {
+      return i;
+      break;
+    }
+  }
 }
 
 export { getTextColor };
