9eee6cc434280f844f1de79eb6e9fbd5283078e1
= <lukehoogenboom@gmail.com>
2024-03-26 08:37:43 +0100

    tsting color recognizion

--- AI/colorClassFinder.js
+++ AI/colorClassFinder.js
@@ -2,87 +2,13 @@ import './Brain.js';
 
 var net = new brain.NeuralNetwork();
 
-net.train([{
-    input: {
-      r: 255,
-      g: 255,
-      b: 255
-    },
-    output: {
-      white: 1
-    }
-  },
-  {
-    input: {
-      r: 240,
-      g: 240,
-      b: 240
-    },
-    output: {
-      white: 1
-    }
-  },
-  {
-    input: {
-      r: 0,
-      g: 1,
-      b: 252
-    },
-    output: {
-      blue: 1
-    }
-  },
-  {
-    input: {
-      r: 56,
-      g: 195,
-      b: 228
-    },
-    output: {
-      lightblue: 1
-    }
-  },
-  {
-    input: {
-      r: 46,
-      g: 211,
-      b: 197
-    },
-    output: {
-      turquose: 1
-    }
-  },
-  {
-    input: {
-      r: 52,
-      g: 168,
-      b: 83
-    },
-    output: {
-      green: 1
-    }
-  },
-  {
-    input: {
-      r: 250,
-      g: 187,
-      b: 8
-    },
-    output: {
-      yellow: 1
-    }
-  },
-  {
-    input: {
-      r: 255,
-      g: 0,
-      b: 0
-    },
-    output: {
-      red: 1
-    }
-  },
-]);
+const trainingData = [
+  { input: [1, 0.5, 0], output: { red: 1 } }, // Red
+  { input: [0, 1, 0], output: { green: 1 } }, // Green
+  { input: [0, 0, 1], output: { blue: 1 } }, // Blue
+  // Add more training examples for other colors
+];
+net.train(trainingData);
 
 function findColorClass (r,g,b) {
     let i;
