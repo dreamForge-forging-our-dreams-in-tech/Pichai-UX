f8069ed48694bfec4e04789dceeada6eaeb8d284
= <lukehoogenboom@gmail.com>
2024-03-26 17:03:35 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -53,15 +53,13 @@ function generateDynamicIcon(image) {
                     colorClass = findColorClass(red, green, blue);
                     console.log(colorClass);
                 } else {
+                    // Replace with your desired color (e.g., green)
+                    imageData.data[i] = colorClass != findColorClass(red, green, blue) ? 255 : rgb[0]; // Red channel
+                    imageData.data[i + 1] = colorClass != findColorClass(red, green, blue) ? 255 : rgb[1]; // Green channel
+                    imageData.data[i + 2] = colorClass != findColorClass(red, green, blue) ? 255 : rgb[2]; // Blue channel
 
                     colorClass = 'themeColor';
                     console.log(colorClass);
-
-                     // Replace with your desired color (e.g., green)
-                     imageData.data[i] = colorClass != findColorClass(red, green, blue) ? 255 : rgb[0]; // Red channel
-                     imageData.data[i + 1] = colorClass != findColorClass(red, green, blue) ? 255 : rgb[1]; // Green channel
-                     imageData.data[i + 2] = colorClass != findColorClass(red, green, blue) ? 255 : rgb[2]; // Blue channel
- 
                 }
             }
             context.putImageData(imageData, 0, 0);
