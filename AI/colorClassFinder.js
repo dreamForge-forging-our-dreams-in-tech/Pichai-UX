080e95fcb226cb4e1e60a72ff2a4e5c55eee5cda
= <lukehoogenboom@gmail.com>
2024-03-26 10:39:11 +0100

    test

--- AI/colorClassFinder.js
+++ AI/colorClassFinder.js
@@ -12,6 +12,26 @@ const trainingData = [
   { input: { r: 0.2, g: 0.5, b: 0.9 }, output: { blue: 1 } },
   { input: { r: 1, g: 1, b: 1 }, output: { white: 1 } }, // Adding white
   { input: { r: 0.9, g: 0.7, b: 0 }, output: { yellow: 1 } }, // Adding yellow
+  { input: { r: 0.8, g: 0.2, b: 0.2 }, output: { red: 1 } },
+  { input: { r: 0.2, g: 0.6, b: 0.3 }, output: { green: 1 } },
+  { input: { r: 0.2, g: 0.5, b: 0.9 }, output: { blue: 1 } },
+  { input: { r: 1, g: 1, b: 1 }, output: { white: 1 } }, // Adding white
+  { input: { r: 0.9, g: 0.7, b: 0 }, output: { yellow: 1 } }, // Adding yellow
+  { input: { r: 0.8, g: 0.2, b: 0.2 }, output: { red: 1 } },
+  { input: { r: 0.2, g: 0.6, b: 0.3 }, output: { green: 1 } },
+  { input: { r: 0.2, g: 0.5, b: 0.9 }, output: { blue: 1 } },
+  { input: { r: 1, g: 1, b: 1 }, output: { white: 1 } }, // Adding white
+  { input: { r: 0.9, g: 0.7, b: 0 }, output: { yellow: 1 } }, // Adding yellow
+  { input: { r: 0.8, g: 0.2, b: 0.2 }, output: { red: 1 } },
+  { input: { r: 0.2, g: 0.6, b: 0.3 }, output: { green: 1 } },
+  { input: { r: 0.2, g: 0.5, b: 0.9 }, output: { blue: 1 } },
+  { input: { r: 1, g: 1, b: 1 }, output: { white: 1 } }, // Adding white
+  { input: { r: 0.9, g: 0.7, b: 0 }, output: { yellow: 1 } }, // Adding yellow
+  { input: { r: 0.8, g: 0.2, b: 0.2 }, output: { red: 1 } },
+  { input: { r: 0.2, g: 0.6, b: 0.3 }, output: { green: 1 } },
+  { input: { r: 0.2, g: 0.5, b: 0.9 }, output: { blue: 1 } },
+  { input: { r: 1, g: 1, b: 1 }, output: { white: 1 } }, // Adding white
+  { input: { r: 0.9, g: 0.7, b: 0 }, output: { yellow: 1 } }, // Adding yellow
   // Add more data as needed
   // Add more data as needed
 ];
