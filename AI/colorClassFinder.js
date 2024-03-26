f7cddeafb4d9d8aa5fe7ea5d5af1ec8f6ebfa512
= <lukehoogenboom@gmail.com>
2024-03-26 09:09:40 +0100

    test

--- AI/colorClassFinder.js
+++ AI/colorClassFinder.js
@@ -28,7 +28,7 @@ function recognizeColor(r, g, b) {
 }
 
 function findColorClass (r,g,b) {
-  return recognizeColor(r,g,b);
+  return recognizeColor(r / 255,g / 255,b / 255);
 }
 
 export { findColorClass };
