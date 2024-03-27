310fc803243b031e5a2bb2f67d5bdf21104a0435
= <lukehoogenboom@gmail.com>
2024-03-27 12:02:29 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -23,7 +23,6 @@ function generateDynamicIcon(image) {
             canvas.height = dynamicImage.height;
 
             context.fillStyle = getComputedStyle(root).getPropertyValue('--primary');
-            alert(context.fillStyle)
             context.fillRect(0, 0, canvas.width, canvas.height);
 
             // Draw the image on the canvas
