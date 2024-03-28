3d2e92c760949388bb72beadaa75dfcf1b942093
= <lukehoogenboom@gmail.com>
2024-03-28 11:54:53 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -64,6 +64,7 @@ function generateDynamicIcon(image) {
             }
 
             context.setTransform(1, 0, 0, 1, 0, 0); // This resets the canvas to its original state
+            context.rotate(-0.1)
             context.putImageData(imageData, 0, 0);
 
             for (let y = 0; y < canvas.height; y++) {
