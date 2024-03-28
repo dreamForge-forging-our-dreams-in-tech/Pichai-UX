316195ddd9f132b6c24e182a1d47873161790fe7
= <lukehoogenboom@gmail.com>
2024-03-28 09:34:46 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -63,7 +63,7 @@ function generateDynamicIcon(image) {
             }
 
             context.setTransform(1, 0, 0, 1, 0, 0);
-            context.rotate(-1);
+            //context.rotate(-1);
             context.putImageData(imageData, 0, 0);
 
             for (let y = 0; y < canvas.height; y++) {
