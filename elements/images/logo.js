ab8620225a9e038da65a4e511d0838f9a15a2d07
= <lukehoogenboom@gmail.com>
2024-03-28 12:07:35 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -64,7 +64,7 @@ function generateDynamicIcon(image) {
             }
 
             context.setTransform(1, 0, 0, 1, 0, 0); // This resets the canvas to its original state
-            context.rotate(-0.4);
+            context.rotate(-0.8);
             context.setTransform(1, 0, 0, 1, 0, 0); // This resets the canvas to its original state
             context.putImageData(imageData, 0, 0);
 
