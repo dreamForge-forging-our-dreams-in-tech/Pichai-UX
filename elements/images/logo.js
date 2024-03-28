ca74def898b5261fe185decfbf5316e7b1823542
= <lukehoogenboom@gmail.com>
2024-03-28 11:09:34 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -64,7 +64,7 @@ function generateDynamicIcon(image) {
             }
 
 
-            context.rotate(1200);
+            context.rotate(800);
             context.drawImage(dynamicImage, -dynamicImage.width / 2, -dynamicImage.height / 2);
             context.putImageData(imageData, 0, 0);
 
