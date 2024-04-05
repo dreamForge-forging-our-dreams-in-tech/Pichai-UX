dc4024d39d64ecea7b2c167929b9d95fd0dc50fa
= <lukehoogenboom@gmail.com>
2024-04-05 13:22:36 +0200

    added card on card color

--- elements/templates/accountDrawer.js
+++ elements/templates/accountDrawer.js
@@ -8,9 +8,11 @@ class AccountDrawer extends HTMLElement {
 
     async connectedCallback() {
         this.innerHTML = `<section class="card" id='drawer'>
+        <article class='card'>
         <h2> Log-In or Sign-Up</h2>
         <p>By logging in with GitHub you agree to Sketch privacy policy and usage of GitHub repo’s and account data.</p>
         <button id="auth"> Authenticate With GitHub</button>
+        </article>
         </section>`;
     }
 }
