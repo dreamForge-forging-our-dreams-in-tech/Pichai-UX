d686a102d72743daf28a3e6d58a6e6e2f9cc219d
= <lukehoogenboom@gmail.com>
2024-04-17 11:02:57 +0200

    test

--- AI/textColorFInder.js
+++ AI/textColorFInder.js
@@ -70,10 +70,13 @@ function getTextColor(color) {
   let b = color[2];
 
   let i;
+  console.log({r: r/255,
+  g: g/255,
+  b: b/255})
   let result = net.run({
-    r: 1.0,
-    g: 1.0,
-    b: 1.0
+    r: r/255,
+    g: g/255,
+    b: b/255
   });
 
   for (i of Object.keys(result)) {
