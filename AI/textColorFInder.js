ead2fa3a04fc700a35ddb0fd82897fa589da0414
= <lukehoogenboom@gmail.com>
2024-04-17 10:52:30 +0200

    test

--- AI/textColorFInder.js
+++ AI/textColorFInder.js
@@ -2,46 +2,7 @@ import './Brain.js';
 
 var net = new brain.NeuralNetwork();
 
-net.train([{
-  input: {
-    r: 0.03,
-    g: 0.7,
-    b: 0.5
-  },
-  output: {
-    black: 1
-  }
-},
-{
-  input: {
-    r: 0.03,
-    g: 0.4,
-    b: 0.5
-  },
-  output: {
-    white: 1
-  }
-},
-{
-  input: {
-    r: 0.16,
-    g: 0.09,
-    b: 0.2
-  },
-  output: {
-    white: 1
-  }
-},
-{
-  input: {
-    r: 0.5,
-    g: 0.5,
-    b: 1.0
-  },
-  output: {
-    white: 1
-  }
-},
+net.train([
 {
   input: {
     r: 61/255,
