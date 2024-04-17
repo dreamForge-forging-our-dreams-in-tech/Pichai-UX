cce15657d452e57b3a59251cd87ec4a64381373e
= <lukehoogenboom@gmail.com>
2024-04-17 10:51:16 +0200

    test

--- init.js
+++ init.js
@@ -71,7 +71,6 @@ class PichaiUX {
             rgb = rgb.replaceAll('a', '').substring(4, rgb.length).replaceAll(')', '');
             rgb = rgb.split(',');
 
-            console.log(rgb)
             i.style.color = getTextColor(rgb);
         }
     }
