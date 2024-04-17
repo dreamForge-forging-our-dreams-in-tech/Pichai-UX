15687a7a183732165ec9d26b1d64db41a1c86a13
= <lukehoogenboom@gmail.com>
2024-04-17 10:55:17 +0200

    test

--- AI/textColorFInder.js
+++ AI/textColorFInder.js
@@ -2,12 +2,41 @@ import './Brain.js';
 
 var net = new brain.NeuralNetwork();
 
-net.train([
+net.train([{
+  input: {
+    r: 0.03,
+    g: 0.7,
+    b: 0.5
+  },
+  output: {
+    black: 1
+  }
+},
 {
   input: {
-    r: 61/255,
-    g: 111/255,
-    b: 141/255
+    r: 0.03,
+    g: 0.4,
+    b: 0.5
+  },
+  output: {
+    white: 1
+  }
+},
+{
+  input: {
+    r: 0.16,
+    g: 0.09,
+    b: 0.2
+  },
+  output: {
+    white: 1
+  }
+},
+{
+  input: {
+    r: 0.5,
+    g: 0.5,
+    b: 1.0
   },
   output: {
     white: 1
@@ -25,9 +54,9 @@ net.train([
 },
 {
   input: {
-    r: 253/255,
-    g: 253/255,
-    b: 253/255
+    r: 1.0,
+    g: 1.0,
+    b: 1.0
   },
   output: {
     black: 1
