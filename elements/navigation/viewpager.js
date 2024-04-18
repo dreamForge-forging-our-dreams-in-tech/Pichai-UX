e25d01c18dfd022407ed349c12ffe4b90dfc22c0
= <lukehoogenboom@gmail.com>
2024-04-18 16:09:40 +0200

    viewpager test 1

---
+++ elements/navigation/viewpager.js
@@ -0,0 +1,24 @@
+
+// Create a class for the element
+class ViewPager extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+    }
+
+    connectedCallback() {
+        let i;
+
+        for(i of this.children) {
+            i.style.visibility = 'hidden';
+        }
+
+        this.children[0].visibility = 'visible';
+
+        this.onwheel = function (e) {
+            console.log(e)
+        }
+    }
+}
+
+customElements.define("view-pager", ViewPager);  
