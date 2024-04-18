5891e874bed27856e1d34afeb267cd40bb72e9b9
= <lukehoogenboom@gmail.com>
2024-04-18 17:29:45 +0200

    test

--- elements/navigation/viewpager.js
+++ elements/navigation/viewpager.js
@@ -18,8 +18,10 @@ class ViewPager extends HTMLElement {
 
         this.onwheel = function (e) {
             this.children[index].style.display = 'none';
-            if (e.deltaY == 100 && (!index == this.children.length)) {
+            if (e.deltaY == 100) {
+                if(index == this.children.length) {} else { //somehow didnt work with one statemnt
                 index++;
+                }
             } else if(!index == 0) {
                 index--;
             }
