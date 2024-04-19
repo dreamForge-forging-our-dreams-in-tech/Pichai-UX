18e02e1409f314700a8d67afe50e4120da89f268
= <lukehoogenboom@gmail.com>
2024-04-19 09:45:44 +0200

    oopsie

--- elements/navigation/viewpager.js
+++ elements/navigation/viewpager.js
@@ -29,7 +29,7 @@ class ViewPager extends HTMLElement {
         this.onwheel = function (e) {
             wheelIndex++;
 
-            if(wheelIndex == ) {
+            if(wheelIndex == sensitivity) {
                 wheelIndex = 0;
 
                 this.children[index].style.display = 'none';
