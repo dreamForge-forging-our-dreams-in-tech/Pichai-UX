a58b05457a7159cc99bdf812c2488dcf4f8b34da
= <lukehoogenboom@gmail.com>
2024-04-19 12:06:27 +0200

    resr

--- elements/navigation/viewpager.js
+++ elements/navigation/viewpager.js
@@ -38,11 +38,11 @@ class ViewPager extends HTMLElement {
                 }
     
                 if(Number(this.getAttribute('pageIndex')) >= this.children.length - 1) {
-                    this.setAttribute('pageIndex', Number(this.getAttribute('looped') == 'true' ? 0 : this.children.length - 1));
+                    this.setAttribute('pageIndex', this.getAttribute('looped') == 'true' ? 0 : this.children.length - 1);
                 }
     
                 if(Number(this.getAttribute('pageIndex')) < 0) {
-                    this.setAttribute('pageIndex', Number(this.getAttribute('looped') == 'true' ? this.children.length - 1 : 0));
+                    this.setAttribute('pageIndex', this.getAttribute('looped') == 'true' ? this.children.length - 1 : 0);
                 }
     
                 //this.children[this.getAttribute('pageIndex')].style.removeProperty('display');
