a565762caafab5fdc5e584e99432aeaa9074cba3
= <lukehoogenboom@gmail.com>
2024-04-19 11:32:55 +0200

    test

--- elements/navigation/viewpager.js
+++ elements/navigation/viewpager.js
@@ -13,12 +13,12 @@ class ViewPager extends HTMLElement {
         let wheelIndex = 0;
         let sensitivity = this.hasAttribute('sensitivity') ? Number(this.getAttribute('sensitivity')) : 4;
 
-        if(!this.pageIndex) {
-            this.pageIndex = 0;
+        if(!this.attributes.pageIndex) {
+            this.setAttribute('pageIndex', 0);
         }
 
         for(i of this.children) {
-            if(this.children[this.pageIndex] == i) {} else {
+            if(this.children[this.attributes.pageIndex] == i) {} else {
             i.style.display = 'none';
             }
         }
@@ -29,39 +29,38 @@ class ViewPager extends HTMLElement {
             if(wheelIndex == sensitivity) {
                 wheelIndex = 0;
 
-                this.children[this.pageIndex].style.display = 'none';
+                this.children[this.attributes.pageIndex].style.display = 'none';
                 if (e.deltaY == 100) {
-                    this.pageIndex = this.pageIndex + 1;
+                    this.attributes.pageIndex = this.attributes.pageIndex + 1;
                 } else {
-                    this.pageIndex = this.pageIndex - 1;
+                    this.attributes.pageIndex = this.attributes.pageIndex - 1;
                 }
     
-                if(this.pageIndex >= this.children.length - 1) {
-                    this.pageIndex = this.getAttribute('looped') == 'true' ? 0 : this.children.length - 1;
+                if(this.attributes.pageIndex >= this.children.length - 1) {
+                    this.attributes.pageIndex = this.getAttribute('looped') == 'true' ? 0 : this.children.length - 1;
                 }
     
-                if(this.pageIndex < 0) {
-                    this.pageIndex = this.getAttribute('looped') == 'true' ? this.children.length - 1 : 0;
+                if(this.attributes.pageIndex < 0) {
+                    this.attributes.pageIndex = this.getAttribute('looped') == 'true' ? this.children.length - 1 : 0;
                 }
     
-                this.children[this.pageIndex].style.removeProperty('display');
+                this.children[this.attributes.pageIndex].style.removeProperty('display');
             }
         }
         }
 
         attributeChangedCallback(name, oldValue, newValue) {
-            if(this.pageIndex >= this.children.length - 1) {
-                this.pageIndex = this.getAttribute('looped') == 'true' ? 0 : this.children.length - 1;
+            if(this.attributes.pageIndex >= this.children.length - 1) {
+                this.attributes.pageIndex = this.getAttribute('looped') == 'true' ? 0 : this.children.length - 1;
             }
 
-            if(this.pageIndex < 0) {
-                this.pageIndex = this.getAttribute('looped') == 'true' ? this.children.length - 1 : 0;
+            if(this.attributes.pageIndex < 0) {
+                this.attributes.pageIndex = this.getAttribute('looped') == 'true' ? this.children.length - 1 : 0;
             }
 
-            alert(oldValue)
             this.children[oldValue].style.display = 'none';
 
-            this.children[this.pageIndex].style.removeProperty('display');
+            this.children[newValue].style.removeProperty('display');
           }
     }
 
