a685be64c95d259c191481a16c9b8ff67d52749c
= <lukehoogenboom@gmail.com>
2024-04-19 11:58:57 +0200

    yrdy

--- elements/navigation/viewpager.js
+++ elements/navigation/viewpager.js
@@ -30,22 +30,14 @@ class ViewPager extends HTMLElement {
             if(wheelIndex == sensitivity) {
                 wheelIndex = 0;
 
-                this.children[this.getAttribute('pageIndex')].style.display = 'none';
+                //this.children[this.getAttribute('pageIndex')].style.display = 'none';
                 if (e.deltaY == 100) {
                     this.setAttribute('pageIndex', Number(this.getAttribute('pageIndex') + 1));
                 } else {
                     this.setAttribute('pageIndex', Number(this.getAttribute('pageIndex') - 1));
                 }
     
-                if(this.getAttribute('pageIndex') >= this.children.length - 1) {
-                    this.setAttribute('pageIndex', Number(this.getAttribute('looped') == 'true' ? 0 : this.children.length - 1));
-                }
-    
-                if(this.getAttribute('pageIndex') < 0) {
-                    this.setAttribute('pageIndex', Number(this.getAttribute('looped') == 'true' ? this.children.length - 1 : 0));
-                }
-    
-                this.children[this.getAttribute('pageIndex')].style.removeProperty('display');
+                //this.children[this.getAttribute('pageIndex')].style.removeProperty('display');
             }
         }
         }
