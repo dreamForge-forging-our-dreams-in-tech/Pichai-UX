e5276f24e089f2492079386868de660dd29937c8
= <lukehoogenboom@gmail.com>
2024-04-19 11:36:46 +0200

    test

--- elements/navigation/viewpager.js
+++ elements/navigation/viewpager.js
@@ -13,7 +13,7 @@ class ViewPager extends HTMLElement {
         let wheelIndex = 0;
         let sensitivity = this.hasAttribute('sensitivity') ? Number(this.getAttribute('sensitivity')) : 4;
 
-        if(!this.getAttribute('pageIndex')) {
+        if(!this.hasAttribute('pageIndex')) {
             this.setAttribute('pageIndex', 0);
         }
 
