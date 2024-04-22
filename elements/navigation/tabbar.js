1367b9c4f95071e480b93129388fb639bd68993b
= <lukehoogenboom@gmail.com>
2024-04-22 11:24:32 +0200

    added verticla tabbar

--- elements/navigation/tabbar.js
+++ elements/navigation/tabbar.js
@@ -1,6 +1,8 @@
 
 // Create a class for the element
 class TabBar extends HTMLElement {
+    static observedAttributes = ["direction"];
+
     constructor() {
         // Always call super first in constructor
         super();
@@ -15,12 +17,20 @@ class TabBar extends HTMLElement {
 
         this.style.gridTemplateColumns = `repeat(` + keys.length + `, 1fr)`;
 
+        if(this.getAttribute('direction') == 'vertical') {
+            this.classList.add('verticalTabbar');
+        }
+
         for (i of keys) {
             if (i.href == window.location.href) {
                 i.classList.add('current');
             }
         }
     }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+
+      }
 }
 
 customElements.define("tab-bar", TabBar);  
