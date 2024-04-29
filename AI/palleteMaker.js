1a9829fad5de4fed4c8236b327bc6c8aab5c179a
= <lukehoogenboom@gmail.com>
2024-04-29 12:09:31 +0200

    container color tests

--- AI/palleteMaker.js
+++ AI/palleteMaker.js
@@ -4,6 +4,37 @@ import { getTextColor } from './textColorFInder.js';
 
 import { PichaiUX } from '../init.js';
 
+function RGBToHSL(value) {
+    let r = value.split(',')[0];
+    let g = value.split(',')[1];
+    let b = value.split(',')[2];
+    // Make r, g, and b fractions of 1
+r /= 255;
+g /= 255;
+b /= 255;
+
+// Find greatest and smallest channel values
+let cmin = Math.min(r,g,b),
+    cmax = Math.max(r,g,b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+
+  // Calculate lightness
+  l = (cmax + cmin) / 2;
+
+  // Calculate saturation
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    
+  // Multiply l and s by 100
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return `${h}, ${s}, ${l}`;
+}
+
 function getPallete(options) {
     return new Promise((resolve) => {
         if (String(options.source).includes('#')) {
@@ -22,6 +53,16 @@ function getPallete(options) {
     });
 }
 
+function generateContainerColor (colors) {
+    let i;
+
+    for(i of colors) {
+        let hsl = RGBToHSL(i);
+        console.log(hsl)
+    }
+
+}
+
 async function generate3ColorPallete(options) {
         let colors = await getPallete(options);
 
@@ -35,6 +76,12 @@ async function generate3ColorPallete(options) {
         root.style.setProperty('--primaryTextColor', getTextColor(colors[0]));
         root.style.setProperty('--secondairyTextColor', getTextColor(colors[4]));
         root.style.setProperty('--tertiaryTextColor', getTextColor(colors[9]));
+
+        let hls = generateContainerColor(colors);
+
+        root.style.setProperty('--primaryContainer', `hsl(${hls[position].toString()})`);
+        root.style.setProperty('--secondairyContainer', `hsl(${hls[position + 4].toString()})`);
+        root.style.setProperty('--tertiaryContainer', `hsl(${hls[position + 9].toString()})`);
 }
 
 export { generate3ColorPallete };
