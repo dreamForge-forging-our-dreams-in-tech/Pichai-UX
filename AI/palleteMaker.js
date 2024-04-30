5486751e7abe73775741c6d3a9aacdb4bd312236
= <lukehoogenboom@gmail.com>
2024-04-30 11:33:05 +0200

    test

--- AI/palleteMaker.js
+++ AI/palleteMaker.js
@@ -17,7 +17,7 @@ function hslToHex(h, s, l) {
 
   function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}` : null;
+    return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}`.split(',') : null;
   }
 
 function RGBToHSL(value) {
