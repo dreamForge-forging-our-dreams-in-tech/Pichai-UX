2aa74b23109da6bee69129c2c63bc179ecb7e581
= <lukehoogenboom@gmail.com>
2024-05-01 11:56:37 +0200

    test

--- elements/navigation/tabbar.js
+++ elements/navigation/tabbar.js
@@ -51,7 +51,7 @@ function addForConnection(e, e2) {
     let element = document.getElementById(e2);
 
     element.addEventListener('pageChange', (e) => {
-        console.log(e.detail.pageIndex)
+        console.log(e.detail)
       });
 }
 
