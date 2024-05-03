99470586d18dcb5e7716b0fa21019af3ad9d362e
= <lukehoogenboom@gmail.com>
2024-05-03 14:50:11 +0200

    removed brown since it falls under theclass of orange

--- AI/colorClassFinder.js
+++ AI/colorClassFinder.js
@@ -23,7 +23,6 @@ const trainingData = [
   { input: { r: 0, g: 0, b: 0.5 }, output: { navy: 1 } },
   { input: { r: 1, g: 0.5, b: 0.3 }, output: { coral: 1 } },
   { input: { r: 0, g: 0.5, b: 0.5 }, output: { teal: 1 } },
-  { input: { r: 0.8, g: 0.1, b: 0.1 }, output: { brown: 1 } },
   { input: { r: 0, g: 0, b: 0 }, output: { black: 1 } },
   { input: { r: (132/255), g: (160/255), b: (160/255) }, output: { white: 1 } },
   { input: { r: (1/255), g: (19/255), b: (34/255) }, output: { black: 1 } },
