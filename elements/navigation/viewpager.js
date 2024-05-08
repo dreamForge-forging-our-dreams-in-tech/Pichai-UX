3abc1c290708c5f03ebc90ad45ca28d0487fd870
= <lukehoogenboom@gmail.com>
2024-05-08 14:04:37 +0200

    test

--- elements/navigation/viewpager.js
+++ elements/navigation/viewpager.js
@@ -11,7 +11,7 @@ class ViewPager extends HTMLElement {
     connectedCallback() {
         let i;
         let wheelIndex = 0;
-        let sensitivity = this.hasAttribute('sensitivity') ? Number(this.getAttribute('sensitivity')) : 6;
+        let sensitivity = this.hasAttribute('sensitivity') ? Number(this.getAttribute('sensitivity')) : 7;
 
         if (!this.hasAttribute('pageIndex')) {
             this.setAttribute('pageIndex', 0);
@@ -29,7 +29,7 @@ class ViewPager extends HTMLElement {
             wheelIndex++;
 
             if (wheelIndex == sensitivity) {
-                wheelIndex = e.deltaY.length > 1 ? -48 : 0;
+                wheelIndex = e.deltaY.length > 1 ? -100 : 0;
 
                 if (e.deltaY > 0) {
                     this.setAttribute('pageIndex', Number(this.getAttribute('pageIndex')) + 1);
