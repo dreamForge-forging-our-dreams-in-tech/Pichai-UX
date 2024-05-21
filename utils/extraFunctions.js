5d779895061bdeadd3f51cd68206253db1918650
= <lukehoogenboom@gmail.com>
2024-05-21 08:54:48 +0200

    test

--- utils/extraFunctions.js
+++ utils/extraFunctions.js
@@ -2,8 +2,6 @@ function extractRgb (i) {
     let rgb = window.getComputedStyle(i)['background-color'];
     rgb = rgb.replaceAll('a', '').substring(4, rgb.length).replaceAll(')', '');
     rgb = rgb.split(',');
-
-    console.log(i)
     
     if(rgb[3]){
         rgb = extractRgb(i.parentNode);
