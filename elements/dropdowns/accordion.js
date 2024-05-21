48be20c072f2244b8512cd413fdcce340fe99059
= <lukehoogenboom@gmail.com>
2024-05-21 14:28:33 +0200

    accordion tests

--- elements/dropdowns/accordion.js
+++ elements/dropdowns/accordion.js
@@ -6,8 +6,18 @@ class Accordion extends HTMLElement {
     }
 
     connectedCallback() {
+        let content = document.createElement('section');
+        let art = document.createElement('article');
+        art.innerHTML = this.innerHTML;
+        this.innerHTML = '';
+
+        content.appendChild(art);
+
         let title = document.createElement('h2');
-        title.innerHTML = this.hasAttribute('')
+        title.innerHTML = this.hasAttribute('titleText') ? this.getAttribute('titleText') : 'Accordion 🪗';
+
+        this.appendChild(title);
+        this.appendChild(content);
     }
 }
 
