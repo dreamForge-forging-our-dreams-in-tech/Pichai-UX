bc4dda9cf1a8136d6dca232b30dd94620bab37f7
= <lukehoogenboom@gmail.com>
2024-05-22 15:23:55 +0200

    wrong property

--- elements/dropdowns/accordion.js
+++ elements/dropdowns/accordion.js
@@ -9,7 +9,7 @@ class Accordion extends HTMLElement {
         let content = document.createElement('section');
         let art = document.createElement('article');
         art.innerHTML = this.innerHTML;
-        art.style.display = this.getAttribute('visible') == 'true' ? 'max-content' : '0px';
+        art.style.height = this.getAttribute('visible') == 'true' ? 'max-content' : '0px';
 
         this.innerHTML = '';
 
