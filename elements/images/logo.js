cc4ccd790d95f981ae755e4dfebd2f70d65769a3
= <lukehoogenboom@gmail.com>
2024-06-03 10:12:38 +0200

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -28,7 +28,7 @@ function generateDynamicIcon(image) {
             context.save();
             // Draw the image on the canvas
             context.translate(canvas.width / 2, canvas.height / 2)
-            context.rotate(0.5);
+            context.rotate(0.55);
             context.drawImage(dynamicImage, -dynamicImage.width / 2, -dynamicImage.height / 2);
 
             // Define the tolerance for color matching (adjust as needed)
