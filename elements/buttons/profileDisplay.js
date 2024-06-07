1d719528f319f0c761b9a74e4d6a1e6bfaf172df
= <lukehoogenboom@gmail.com>
2024-06-07 12:48:11 +0200

    fixed an issue where the profile display button had code of the x-icon element

--- elements/buttons/profileDisplay.js
+++ elements/buttons/profileDisplay.js
@@ -1,7 +1,6 @@
 import { registry } from '../../utils/customeElementsDefine.js';
 // Create a class for the element
 class DisplayProfile extends HTMLElement {
-    static observedAttributes = ["src", "dynamic"];
 
     constructor() {
         // Always call super first in constructor
@@ -21,23 +20,6 @@ class DisplayProfile extends HTMLElement {
             }
             document.body.appendChild(dialog);
         }
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-                // Get the favicon link element
-                const faviconLink = document.querySelector("link[rel='icon']") || document.querySelector("link[rel='shortcut icon']");
-
-                // Get the favicon URL
-                const faviconUrl = faviconLink ? faviconLink.href : null;
-        
-                if (!this.hasAttribute('dynamic') || this.getAttribute('dynamic') == 'true') {
-                    window.onload = async () => {
-                        let newIcon = await generateDynamicIcon(faviconUrl);
-                        this.style.backgroundImage = `url(${newIcon})`;
-                    }
-                } else {
-                    this.style.backgroundImage = this.src ?? `url("${faviconUrl}")`;
-                }
     }
 }
 
