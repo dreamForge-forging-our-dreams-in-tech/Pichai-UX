c11a2a45878fda32518412078fb9bfd9785b730f
= <lukehoogenboom@gmail.com>
2024-06-07 14:09:38 +0200

    test

--- utils/customeElementsDefine.js
+++ utils/customeElementsDefine.js
@@ -26,7 +26,11 @@ function extractAttributes (constructor) {
         con = con.substring(con.indexOf('static observedAttributes = ['), con.indexOf(']'));
         con = con.replaceAll('static observedAttributes = [','').replace(/['"]+/g, '').split(',');
 
-        return extractAttributeTypes(constructor, con.reduce((acc,curr)=> (acc[curr]='all',acc),{}));
+        const obj = con.reduce((accumulator, value) => {
+          return {...accumulator, [value]: ''};
+        }, {});
+
+        return extractAttributeTypes(constructor, obj);
     }
 
     return {};
