8a748bd25c0f448a6c6850fed443c91e1b6a6e54
Chris van der geest <chrisvandergeest14@gmail.com>
2024-06-13 15:54:37 +0200

    google i hope ur pillow is warm tonight

--- elements/templates/footer.js
+++ elements/templates/footer.js
@@ -12,7 +12,7 @@ class Footer extends HTMLElement {
         <x-icon></x-icon>
         <x-title></x-title>
         <x-icon src='https://banner2.cleanpng.com/20180502/eve/kisspng-discord-computer-icons-logo-simplify-5aea4b0b9a5315.3824979315253040756321.jpg'></x-icon>
-        <x-icon src='https://i.pinimg.com/736x/7d/dc/54/7ddc545046b212d9ecc8eef83569222b.jpg'></x-icon>
+        <x-icon src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/discord.png'></x-icon>
         <x-icon src='https://w7.pngwing.com/pngs/69/215/png-transparent-social-media-twitter-blog-user-pajarito-computer-network-text-logo-thumbnail.png'></x-icon>
     </footer>`;
     }
