487a4d8a3a278043bf02aad2e2b5dee8f305bf32
Chris van der geest <chrisvandergeest14@gmail.com>
2024-06-13 15:58:49 +0200

    kut google

--- elements/templates/footer.js
+++ elements/templates/footer.js
@@ -11,9 +11,9 @@ class Footer extends HTMLElement {
         this.innerHTML = `<footer>
         <x-icon></x-icon>
         <x-title></x-title>
-        <x-icon src='https://banner2.cleanpng.com/20180502/eve/kisspng-discord-computer-icons-logo-simplify-5aea4b0b9a5315.3824979315253040756321.jpg'></x-icon>
         <x-icon src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/discord.png'></x-icon>
-        <x-icon src='https://w7.pngwing.com/pngs/69/215/png-transparent-social-media-twitter-blog-user-pajarito-computer-network-text-logo-thumbnail.png'></x-icon>
+        <x-icon src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/discord.png'></x-icon>
+        <x-icon src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/discord.png'></x-icon>
     </footer>`;
     }
 }
