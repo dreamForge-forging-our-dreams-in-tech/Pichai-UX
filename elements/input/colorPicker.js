c7ae8a940001225c1d23d801fb593c67dd624c4d
= <lukehoogenboom@gmail.com>
2024-06-19 12:04:10 +0200

    test

--- elements/input/colorPicker.js
+++ elements/input/colorPicker.js
@@ -258,8 +258,7 @@ function updateColors (el, color) {
     b.value = parseInt(rgb[2]);
 
     if(document.getElementById('a')) {
-        console.log(parseInt(rgb[3]));
-        document.getElementById('a').value = parseInt(rgb[3]);
+        document.getElementById('a').value = isNaN(parseInt(rgb[3])) ? 1 : parseInt(rgb[3]);
     }
 
     //el.parentNode.setAttribute('previousvalue', el.parentNode.getAttribute('value')); // make this update depending on set outputtype
