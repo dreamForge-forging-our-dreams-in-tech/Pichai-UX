fd8fe0f2654ea49e84a884e61f029bc9484a23fc
= <lukehoogenboom@gmail.com>
2024-06-19 11:17:16 +0200

    typo

--- elements/input/colorPicker.js
+++ elements/input/colorPicker.js
@@ -238,7 +238,7 @@ function createSlider(value, el) {
     let slider = document.createElement('input');
     slider.type = 'range';
     slider.min = '0';
-    slider.steps = '0.1';
+    slider.step = '0.1';
     slider.max = value === 'a' ? 1 : 255;
     slider.id = value;
 
