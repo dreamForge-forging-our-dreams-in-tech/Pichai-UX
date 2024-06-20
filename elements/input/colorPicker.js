a5b4b022f3a1369f09dd1f2d8f87b281b93215ee
= <lukehoogenboom@gmail.com>
2024-06-20 11:38:15 +0200

    test

--- elements/input/colorPicker.js
+++ elements/input/colorPicker.js
@@ -264,10 +264,9 @@ function updateColors (el, color) {
 
     if(document.getElementById('a')) {
         document.getElementById('a').value = isNaN(parseInt(rgb[3])) ? 1 : parseInt(rgb[3]);
-        console.log(a.value)
     }
 
-    console.log(a.value)
+    console.log(document.getElementById('a').value)
 
     el.setAttribute('value', `rgba(${r.value}, ${g.value}, ${b.value}, ${a.value})`);
 }
