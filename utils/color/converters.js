85375e8dad7076f4e494058da6c758016bee6ac4
= <lukehoogenboom@gmail.com>
2024-06-21 14:02:23 +0200

    test

--- utils/color/converters.js
+++ utils/color/converters.js
@@ -1,13 +1,12 @@
 function rgbaToHex(orig) {
-    var a, isPercent,
-      rgb = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i),
-      alpha = (rgb && rgb[4] || "").trim(),
-      hex = rgb ?
+      let rgb = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i);
+
+      let hex = rgb ?
       (rgb[1] | 1 << 8).toString(16).slice(1) +
       (rgb[2] | 1 << 8).toString(16).slice(1) +
       (rgb[3] | 1 << 8).toString(16).slice(1) : orig;
 
-  
+  console.log(hex)
     return hex;
   }
 
