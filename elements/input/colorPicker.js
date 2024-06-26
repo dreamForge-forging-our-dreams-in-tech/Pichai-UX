5338a1911686101dc7496165313d10d94c273d80
= <lukehoogenboom@gmail.com>
2024-06-26 11:40:51 +0200

    test

--- elements/input/colorPicker.js
+++ elements/input/colorPicker.js
@@ -60,6 +60,7 @@ class ColorPicker extends HTMLElement {
             updateColors(this, prev[0].style.backgroundColor); // use backgroundColor so we dont need to use conversion functions for colors and can just make the code les mumbo jumbo
 
             if (oldValue != newValue) {
+                console.log('test')
                 updateToOutputType(this);
 
                 this.setAttribute('previousvalue', oldValue);
