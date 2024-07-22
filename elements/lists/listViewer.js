fa18781e0f96184b3fd3d6735a04ffdc96843c03
= <lukehoogenboom@gmail.com>
2024-07-22 14:05:31 +0200

    test

--- elements/lists/listViewer.js
+++ elements/lists/listViewer.js
@@ -1,9 +1,11 @@
 import { registry, doAttributeCheck } from '../../utils/customeElementsDefine.js';
 
+const click = new Event("click"); //fires when an item in the listViewer is clicked, returns the text of the clicked item.
+
 // Create a class for the <log-in> element
 class ListViewer extends HTMLElement {
     /** @description 
-    * Displays a set of items below each other, these items can be interacted with by the user.
+    * The list-viewer element allows you to display a set of items below each other, these items can be interacted with by the user.
     */
 
     /** @usage 
