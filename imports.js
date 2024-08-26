e2e599dea878a9e946b1139bee812cdc31325e02
= <lukehoogenboom@gmail.com>
2024-08-26 15:17:21 +0200

    forgot the import

--- imports.js
+++ imports.js
@@ -20,6 +20,8 @@ import './elements/input/colorPicker.js';
 
 import './elements/lists/listViewer.js';
 
+import './elements/signs/note.js';
+
 import './elements/templates/footer.js';
 import './elements/templates/header.js';
 import './elements/templates/login.js';
