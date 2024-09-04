2847a8074fff31686dbf45adef9b827f33e99ebb
= <lukehoogenboom@gmail.com>
2024-09-04 08:35:03 +0200

    test

--- utils/elements/supportForms.js
+++ utils/elements/supportForms.js
@@ -1,7 +1,18 @@
 // this is a helper function to help us create custom input elements that will work with a form
 
 function supportForm() {
-    this.style.backgroundColor = 'yellow';
+    let formInput = document.createElement('input'); // creates an input who it's value changes when the custom input element does
+    formInput.classList.add('hiddenFormInput');
+
+    if(this.hasAttribute('name')) {
+        formInput.setAttribute('name', this.hasAttribute('name'));
+    }
+
+    window.setInterval(() => {
+        formInput.value = this.getAttribute('value');
+    });
+
+    this.append(formInput);
 }
 
 // Attach the function to the HTMLElement prototype
