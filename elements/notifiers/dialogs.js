f0d492d4ce1a6b97ce98fcef8a9b60cbf0cf5012
= <lukehoogenboom@gmail.com>
2024-09-04 09:57:24 +0200

    test

---
+++ elements/notifiers/dialogs.js
@@ -0,0 +1,38 @@
+// this functions adds the possibility to turn all elements into a dialog
+// it simply creates a clone that will be shown in the dialog
+
+/** @description 
+* A Dialog is a powerfull way of notifying the user on important matters.
+*/
+
+/** @usage 
+ * Used for showing bits of information, notifying or having an user select something
+*/
+
+/** @codeUsage 
+* You can call the showAsDialog function on all elements, using the setDialogMode attribute of the element allwos you to set how the element reacts and works with this function
+*/
+
+function showAsDialog(clone) {
+    let wrapper = document.createElement('article');
+    wrapper.classList.add('dialogWrapper');
+
+    let dialogForm = document.createElement('form'); // creates an input who it's value changes when the custom input element does
+    dialogForm.classList.add('dialog');
+
+    wrapper.append(dialogForm);
+
+    document.body.append(wrapper);
+}
+
+function setDialogMode(mode) { // sets the elements mode for a dialog pop-up
+    if(mode == 'clone') {
+        this.style.removeProperty('display');
+    } else {
+        this.style.display = 'none';
+    }
+}
+
+// Attach the function to the HTMLElement prototype
+HTMLElement.prototype.showAsDialog = showAsDialog;
+HTMLElement.prototype.dialogMode = setDialogMode; // can be either way clone to clone the element into a dialog or dialog wich hides the element and shows it in a dialog
