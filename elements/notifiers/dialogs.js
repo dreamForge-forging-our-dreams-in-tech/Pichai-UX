d164b44cb4f43c3ea627a1051db4ccfecfe43add
= <lukehoogenboom@gmail.com>
2024-09-06 09:57:41 +0200

    test

--- elements/notifiers/dialogs.js
+++ elements/notifiers/dialogs.js
@@ -55,7 +55,7 @@ function showAsDialog(clone, cancelable = true, titleText = 'Dialog') { // turns
     let children = this.dialogMode == 'dialog' ? this : this.cloneNode(true);
     dialogForm.append(children);
 
-    console.log(window.getComputedStyle(dialogForm));
+    console.log(window.getComputedStyle(dialogForm).width);
     titleWrapper.style.width = window.getComputedStyle(dialogForm)['width'];
 
     wrapper.append(titleWrapper, dialogForm); // append all items to the dialog wrapper
