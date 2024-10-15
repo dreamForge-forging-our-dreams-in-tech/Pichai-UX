31e47b81da1797c13d06540afaf66d753fb8ddd6
luke hoogenboom <6035070@mborijnland.nl>
2024-10-15 13:53:51 +0200

    test

--- elements/globalElementFunctions/setListItems.js
+++ elements/globalElementFunctions/setListItems.js
@@ -1,6 +1,6 @@
 let i;
 
-function enableSetListItems(element,callback) {
+function enableSetListItems(element,callback) { // allows user to set the contents of a listview with js code, after finishing a callback is fired - required
     let listItems = [];
 
     // Initialize listItems with the current children
