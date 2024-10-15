fcabb9c005279fcbe047afca0d0053700d8ec35c
luke hoogenboom <6035070@mborijnland.nl>
2024-10-15 11:39:02 +0200

    test

--- elements/globalElementFunctions/setListItems.js
+++ elements/globalElementFunctions/setListItems.js
@@ -4,27 +4,9 @@ function enableSetListItems(element) {
     for (i of this.children) {
         if (i.tagName == 'HR') { } else {
             let text = String(i.innerHTML)
-            this.listItems.push(text.substring(0, text.indexOf('<'))); //adds the items to the listItems object for setting/getting
+            this.listItems.push(text); //adds the items to the listItems object for setting/getting
         }
     }
-
-    // Options for the observer (which mutations to observe)
-    const config = { attributes: true };
-
-    // Callback function to execute when mutations are observed
-    const callback = (mutationList, observer) => {
-        for (const mutation of mutationList) {
-            if (mutation.type === "attributes") {
-                console.log(`The ${mutation.attributeName} attribute was modified.`);
-            }
-        }
-    };
-
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
-
-    // Start observing the target node for configured mutations
-    observer.observe(this, config);
 }
 
 export { enableSetListItems }
