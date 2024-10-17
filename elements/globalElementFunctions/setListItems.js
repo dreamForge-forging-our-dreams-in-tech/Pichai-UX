d0fa2c9d6c84b0a5024d2cef6aad059131bf64a5
luke hoogenboom <6035070@mborijnland.nl>
2024-10-17 15:01:37 +0200

    test

--- elements/globalElementFunctions/setListItems.js
+++ elements/globalElementFunctions/setListItems.js
@@ -18,18 +18,17 @@ function enableSetListItems(element, callback) { // allows user to set the conte
             return listItems;
         },
         set(newValue) {
-            if(newValue) {
-                element.classList.add('Hidden');
-            } else {
-                element.classList.remove('Hidden');
-            }
-            
             if (element.children.length == 0) {
                 element.firstTime = true;
             }
 
             if (!element.firstTime) { } else { // checks if it is set for the firsttime, if so ignore changes
                 element.innerHTML = '';
+                if (newValue) {
+                    element.classList.add('Hidden');
+                } else {
+                    element.classList.remove('Hidden');
+                }
 
                 for (i of newValue) {
                     let a = document.createElement(i == 'HR' ? 'hr' : 'li');
