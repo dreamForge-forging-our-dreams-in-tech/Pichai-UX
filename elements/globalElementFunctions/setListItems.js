ec96b8dd951ddde78a21f60b79e965d7052774b2
luke hoogenboom <6035070@mborijnland.nl>
2024-10-17 14:34:17 +0200

    test

--- elements/globalElementFunctions/setListItems.js
+++ elements/globalElementFunctions/setListItems.js
@@ -18,7 +18,11 @@ function enableSetListItems(element, callback) { // allows user to set the conte
             return listItems;
         },
         set(newValue) {
-            if (!element.firstTime) { } else { // checks if it is set for the firsttime, if so ignore changes
+            if(element.children.length == 0) {
+                element.firstTime = true;
+            }
+
+            if (element.firstTime) { } else { // checks if it is set for the firsttime, if so ignore changes
                 element.innerHTML = '';
 
                 for (i of newValue) {
