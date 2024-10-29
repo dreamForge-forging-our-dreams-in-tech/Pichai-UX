09b543d7b8d9ec5a5238353e407b7211b34b5836
luke hoogenboom <6035070@mborijnland.nl>
2024-10-29 10:57:20 +0100

    test

--- elements/templates/settings.js
+++ elements/templates/settings.js
@@ -66,14 +66,14 @@ class customSettings extends HTMLElement {
 
                 window.parent.location.reload();
             } else if (e.detail.value == 'reset') {
-                if (await parent.showConfirmDialog('Are you sure?', 'Are you sure you want to clear all data?')) {
+                if (await window.parent.showConfirmDialog('Are you sure?', 'Are you sure you want to clear all data?')) {
                     for (i in localStorage) {
                         if (String(i).includes(window.storageName)) {
                             window.localStorage.removeItem(i);
                         }
                     }
 
-                    await parent.showAlertDialog('Data cleared', 'Please refresh your window to finalize reset.');
+                    await window.parent.showAlertDialog('Data cleared', 'Please refresh your window to finalize reset.');
                 }
             } else if (e.detail.value == 'Fonts') {
 
