e402148733077d9a8c40e59ca6acec4646c9672f
luke hoogenboom <6035070@mborijnland.nl>
2024-10-29 10:00:16 +0100

    test

--- elements/templates/footer.js
+++ elements/templates/footer.js
@@ -11,9 +11,20 @@ class Footer extends HTMLElement {
         this.innerHTML = `<footer>
         <x-icon></x-icon>
         <x-title title='Sketch'></x-title>
-        <x-icon src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/twitter.png'></x-icon>
-        <x-icon src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/discord.png'></x-icon>
-        <x-icon src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/youtube.png'></x-icon>
+    <a href="https://x.com/Sketch68862592?t=PpUXmuphCIXxY0wCdO33Ng&s=08">
+      <x-icon
+        src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/twitter.png'></x-icon>
+    </a>
+
+    <a href="https://discord.gg/2tJvuH73Kq">
+      <x-icon
+        src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/discord.png'></x-icon>
+    </a>
+
+    <a href="https://www.youtube.com/@sketch8168">
+      <x-icon
+        src='https://raw.githubusercontent.com/lukeplays33/Pichai-UX/main/assets/shapes/pictures/youtube.png'></x-icon>
+    </a>
     </footer>`;
     }
 }
