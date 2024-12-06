0df45aee0855a082aec2e3c57bf0c3c11459f866
luke hoogenboom <6035070@mborijnland.nl>
2024-12-06 13:34:13 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -149,8 +149,10 @@ class Logo extends HTMLElement {
         }
 
         if ((!this.hasAttribute('dynamic') || this.getAttribute('dynamic') == 'true') && !dynamicGeneratedIcons.hasOwnProperty(faviconUrl)) {
-            //let newIcon = await generateDynamicIcon(faviconUrl, parseInt(radius));
-            //this.style.backgroundImage = `url(${newIcon})`;
+            let newIcon = await generateDynamicIcon(faviconUrl, parseInt(radius));
+            this.style.backgroundImage = `url(${newIcon})`;
+            
+            dynamicGeneratedIcons[faviconUrl] = `url(${newIcon})`;
 
             window.onload = async () => {
                 let newIcon = await generateDynamicIcon(faviconUrl, parseInt(radius));
