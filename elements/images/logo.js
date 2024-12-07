7666a1c690a76d84be296efd003fcb16242b8a2e
luke hoogenboom <6035070@mborijnland.nl>
2024-12-07 17:45:29 +0100

    tewest

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -66,7 +66,7 @@ function generateDynamicIcon(image, radius = 360) {
                 }
             }
 
-                        context.clearRect(0, 0, canvas.width, canvas.height);
+            context.clearRect(0, 0, canvas.width, canvas.height);
             context.fillRect(0, 0, canvas.width, canvas.height);
 
             // Define the tolerance for color matching (adjust as needed)
