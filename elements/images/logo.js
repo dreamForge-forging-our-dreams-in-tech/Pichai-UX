99306dbc194a24d2b2e62bf31a56ba3ca5d3fee6
luke hoogenboom <6035070@mborijnland.nl>
2024-12-07 20:57:41 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -31,8 +31,7 @@ function generateDynamicIcon(image, radius = 360) {
 
             context.save();
             // Draw the image on the canvas
-            context.translate(canvas.width / 2, canvas.height / 2)
-            context.rotate(0.55);
+            context.translate(canvas.width / 2, canvas.height / 2);
             context.drawImage(dynamicImage, -dynamicImage.width / 2, -dynamicImage.height / 2);
 
             // Get the entire image data as an array of pixel data
@@ -42,10 +41,7 @@ function generateDynamicIcon(image, radius = 360) {
             let colorClass;
 
             context.setTransform(1, 0, 0, 1, 0, 0); // This resets the canvas to its original state
-
-            context.translate(-canvas.width / 5.0, canvas.height / 3.0);
-            context.rotate(-0.55);
-            context.putImageData(imageData, 0, 0);
+            context.translate(-3, -3);
 
             
             for (let y = 0; y < canvas.height; y++) {
