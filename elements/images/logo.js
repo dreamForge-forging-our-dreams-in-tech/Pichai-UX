29fa3efb8f5032c67329c46fd34e5939a6b483bb
luke hoogenboom <6035070@mborijnland.nl>
2024-12-16 12:02:43 +0100

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -74,13 +74,14 @@ async function generateDynamicIcon(image, radius = 360) {
                     if (colorClass != findColorClass(red, green, blue)) {
                         // Replace the pixel with a 5x5 square
                         context2.fillStyle = textColor == 255 ? 'white' : 'black'; // Set your desired color here
-                        context2.fillRect(x, y, 80, 80); // Draw a 5x5 square
+                        context2.fillRect(x, y, 4, 4); // Draw a 5x5 square
 
                         colorClass = findColorClass(red, green, blue);
                     }
                 }
             }
 
+            console.log(canvas2.toDataURL())
             context.drawImage(canvas2, 0, 0); 
 
             context.strokeStyle = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
