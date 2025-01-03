f913cb522803ba4d26d65224005b79f25786b9a3
luke hoogenboom <6035070@mborijnland.nl>
2025-01-03 13:33:39 +0100

    now reclicking a tabbar item will no longer fire extra click events

--- elements/navigation/tabbar.js
+++ elements/navigation/tabbar.js
@@ -107,13 +107,14 @@ function addForConnection(e, e2) {
             e.preventDefault();
 
             if (!this.classList.contains('current')) {
-                e.stopImmediatePropagation(); // to prevent other listeners to fire if already selected
                 element.setAttribute('pageIndex', Number(this.getAttribute('index')));
 
                 try {
                     i.parentNode.getElementsByClassName('current')[0].classList.remove('current');
                 } catch (e) { }
                 this.classList.add('current');
+            } else {
+                e.stopImmediatePropagation(); // to prevent other listeners to fire if already selected
             }
         });
     }
