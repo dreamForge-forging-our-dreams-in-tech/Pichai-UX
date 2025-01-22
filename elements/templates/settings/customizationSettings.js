c9aa176ac0eb8df749b768cbb0b2689610baf73b
luke hoogenboom <6035070@mborijnland.nl>
2025-01-22 11:47:25 +0100

    testing wallpaper customization

--- elements/templates/settings/customizationSettings.js
+++ elements/templates/settings/customizationSettings.js
@@ -14,37 +14,11 @@ class customSettings extends HTMLElement {
     async connectedCallback() {
         let i;
 
-        let value = window.localStorage.getItem(`${window.storageName}transperncy`) == null ? '1' : window.localStorage.getItem(`${window.storageName}transperncy`);
-        let checked = value == '1' ? '' : 'checked';
-
-        let rtlValue = window.localStorage.getItem(`${window.storageName}rtl`) == null ? 'false' : window.localStorage.getItem(`${window.storageName}rtl`);
-        let rtlChecked = rtlValue == 'false' ? '' : 'checked';
-
-        this.innerHTML = `<list-viewer actionButton="${window.default_edit_icon}">
-        <hr titleText="Appearance">
-                <li id='wallpaper'>Wallpaper</li>
-                <li>Theme Colors</li>
-                <li>Color Order</li>
-                <li id='trans' >Transparency Mode <input id='Pichai - transperencyMode' type='checkbox' class='actionButton' ${checked}></input></li>
-
-                <hr titleText="Borders">
-                <li>Border style</li>
-                <li>Border Radius</li>
-
-                <hr titleText="Text and Fonts">
-                <li>Font</li>
-                <li id='rtl' >RTL Mode <input id='Pichai - transperencyMode' type='checkbox' class='actionButton' ${rtlChecked}></input></li>
-
-                <hr titleText="Icons">
-                <li>Icon packs</li>
-                <li>Icon shape</li>
-                <li>Themed Icons</li>
-
-                <hr titleText="Data">
-                <li id='download'>Download Theme</li>
-                <li id='upload'>Upload Theme</li>
-                <li id='reset'>Reset data</li>
-            </list-viewer>`;
+        this.innerHTML = `<section class="card" id='quickSettingsPanel'>
+        <list-viewer sortable='true' id='QSP'>
+        <li id='Wallpaper' >Wallpaper</li>
+        </list-viewer>
+        </section>`;
 
         this.firstChild.addEventListener('itemSelected', async function (e) {
             if (e.detail.value == 'wallpaper') {
@@ -53,53 +27,6 @@ class customSettings extends HTMLElement {
 
                     window.parent.location.reload();
                 });
-            } else if (e.detail.value == 'trans') {
-                let li = document.getElementById(e.detail.value);
-
-                li.children[0].checked = !li.children[0].checked;
-
-                if (li.children[0].checked) {
-                    window.localStorage.setItem(`${window.storageName}transperncy`, 0.75);
-                } else {
-                    window.localStorage.setItem(`${window.storageName}transperncy`, 1);
-                }
-
-                window.parent.location.reload();
-            } else if (e.detail.value == 'reset') {
-                if (await showConfirmDialog('Are you sure?', 'Are you sure you want to clear all data?')) {
-                    for (i in localStorage) {
-                        if (String(i).includes(window.storageName)) {
-                            window.localStorage.removeItem(i);
-                        }
-                    }
-
-                    await showAlertDialog('Data cleared', 'Please refresh your window to finalize reset.');
-                }
-            } else if (e.detail.value == 'Fonts') {
-
-            } else if (e.detail.value == 'rtl') {
-                let li = document.getElementById(e.detail.value);
-
-                li.children[0].checked = !li.children[0].checked;
-
-                if (li.children[0].checked) {
-                    window.localStorage.setItem(`${window.storageName}rtl`, 'true');
-                } else {
-                    window.localStorage.setItem(`${window.storageName}rtl`, 'false');
-                }
-
-                window.parent.location.reload();
-            } else if (e.detail.value == 'download') {
-                let li = document.getElementById(e.detail.value);
-
-                li.children[0].checked = !li.children[0].checked;
-
-                if (li.children[0].checked) {
-                    window.localStorage.setItem(`${window.storageName}rtl`, 'true');
-                } else {
-                    window.localStorage.setItem(`${window.storageName}rtl`, 'false');
-                }
-
             }
         });
     }
