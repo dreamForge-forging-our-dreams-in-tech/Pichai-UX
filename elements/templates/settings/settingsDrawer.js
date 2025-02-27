15af3a9b7671c45d344ddc40122aeefda7edc5f2
luke hoogenboom <luke@technolableiden.nl>
2025-02-27 10:22:07 +0100

    added a wallpaper inversion filter to allow some dark mode control

--- elements/templates/settings/settingsDrawer.js
+++ elements/templates/settings/settingsDrawer.js
@@ -34,7 +34,7 @@ function detectCustomization(e) {
         window.localStorage.setItem(`${window.storageName}rotation`, Number(window.prompt('Enter the hue-rotation \nHue-Rotation allows you so set custom colors for your elements, colors wich are not taken from the wallpaper.', 1)));
 
     }  else if (e.detail.value == 'Wallpaper Inversion') {
-        window.localStorage.setItem(`${window.storageName}wInversion`, Number(window.prompt('Enter the color inversion for the wallpaper (0-1)', 1)));
+        window.localStorage.setItem(`${window.storageName}wInversion`, Number(window.prompt('Enter the color inversion for the wallpaper (0-1) \nTHis filter can be overwritten by the "Inversion" filter, values may need alteration if used in combination.', 1)));
         
     }  else if (e.detail.value == 'Inversion') {
         let value = Number(window.prompt('Enter the color inversion for the website (0 - 1)', 1))
