1c3c2a713c45c526cfe0bd361fb70b00d87767fb
luke hoogenboom <luke@technolableiden.nl>
2025-03-13 14:10:12 +0100

    test

--- elements/dropdowns/accordion.js
+++ elements/dropdowns/accordion.js
@@ -2,9 +2,9 @@ import { registry, doAttributeCheck } from '../../utils/customeElementsDefine.js
 
 // Create a class for the element
 class Accordion extends HTMLElement {
-        /** @description 
-    * The accordion element allows you to hide or show content.
-    */
+    /** @description 
+* The accordion element allows you to hide or show content.
+*/
 
     /** @usage 
      * Hide less important information
@@ -19,7 +19,7 @@ class Accordion extends HTMLElement {
 
     connectedCallback() {
         let content = document.createElement('section');
-        
+
         let art = document.createElement('article');
         art.innerHTML = this.innerHTML;
         this.innerHTML = '';
@@ -36,7 +36,7 @@ class Accordion extends HTMLElement {
         arrow.onclick = function (e) {
             e.stopPropagation();
 
-            if(art.style.height == '100%') {
+            if (art.style.height == '100%') {
                 art.style.height = '0px';
                 art.style.padding = '0px 8px 0px 8px';
 
@@ -55,7 +55,7 @@ class Accordion extends HTMLElement {
             }
         }
 
-        if(this.getAttribute('open') == 'true') {
+        if (this.getAttribute('open') == 'true') {
             art.style.height = '100%';
             art.style.padding = '8px';
 
@@ -75,7 +75,7 @@ class Accordion extends HTMLElement {
         });
 
         title.appendChild(arrow);
-        
+
         this.appendChild(title);
         this.appendChild(content);
     }
@@ -84,32 +84,34 @@ class Accordion extends HTMLElement {
         doAttributeCheck('string', 'titletext', this.getAttribute('titletext'));
         doAttributeCheck('boolean', 'open', this.getAttribute('open'));
 
-        if(name == 'titletext') { // lets you change the title of an accordion
+        if (name == 'titletext') { // lets you change the title of an accordion
             this.children[0].innerHTML = this.children[0].innerHTML.replace(oldValue, newValue);
 
         } else if (name == 'open') {
-            let art = this.children[1].firstChild;
-            let arrow = this.children[0].children[0];
+            try {
+                let art = this.children[1].firstChild;
+                let arrow = this.children[0].children[0];
 
-            if(newValue == 'false') {
-                art.style.height = '0px';
-                art.style.padding = '0px 8px 0px 8px';
+                if (newValue == 'false') {
+                    art.style.height = '0px';
+                    art.style.padding = '0px 8px 0px 8px';
 
-                art.parentNode.classList.add('hidden');
-                art.parentNode.parentNode.classList.add('hiddenAccordion');
+                    art.parentNode.classList.add('hidden');
+                    art.parentNode.parentNode.classList.add('hiddenAccordion');
 
-                arrow.innerHTML = 'arrow_drop_down';
-            } else {
-                art.style.height = '100%';
-                art.style.padding = '8px';
+                    arrow.innerHTML = 'arrow_drop_down';
+                } else {
+                    art.style.height = '100%';
+                    art.style.padding = '8px';
 
-                art.parentNode.classList.remove('hidden');
-                art.parentNode.parentNode.classList.remove('hiddenAccordion');
+                    art.parentNode.classList.remove('hidden');
+                    art.parentNode.parentNode.classList.remove('hiddenAccordion');
 
-                arrow.innerHTML = 'arrow_drop_up';
-            }
+                    arrow.innerHTML = 'arrow_drop_up';
+                }
+            } catch (e) { }
         }
-}
+    }
 }
 
 registry.define("accordion-dropdown", Accordion);  
