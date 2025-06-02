f9da7ac11eb52c5b743f421364cfb640300793c9
Luke-technolab <luke@technolableiden.nl>
2025-06-02 15:03:07 +0200

    test

--- AI/palleteMaker.js
+++ AI/palleteMaker.js
@@ -67,7 +67,6 @@ function RGBToHSL(value) {
 }
 
 function getPallete(options) {
-    console.log(options.source)
     return new Promise((resolve) => {
         if (options.source.length == 1) { // for generation from a single supplied color
 
