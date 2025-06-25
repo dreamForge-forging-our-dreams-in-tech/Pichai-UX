773f550e535ef813e9507ffe265dc1773ecb00c3
dreamForge <54947833+lukeplays33@users.noreply.github.com>
2025-06-25 19:36:39 +0200

    test

--- AI/palleteMaker.js
+++ AI/palleteMaker.js
@@ -159,6 +159,9 @@ async function generate3ColorPallete(options) {
         window.wallpaper = document.body.style.backgroundImage;
         document.body.style.backgroundImage = 'none'; // removes the background image so that the hue rotation can be applied correctly
     } else {
+        if(window.wallpaper) {
+            window.wallpaper = document.body.style.backgroundImage
+        }
         document.body.style.removeProperty('background-image');
 
         let image = String(window.wallpaper);
