f76b2a49edc14b02e8b7c8c36b586239918a896e
dreamForge <54947833+lukeplays33@users.noreply.github.com>
2025-06-25 19:46:34 +0200

    test

--- init.js
+++ init.js
@@ -66,6 +66,8 @@ class PichaiUX {
 
         if (window.localStorage.getItem(`${window.storageName}bgImageChange`)) {
             image = window.localStorage.getItem(`${window.storageName}bgImageChange`);
+        } else {
+            window.sessionStorage.setItem(`${window.storageName}bgImageChange`, image); // extra safe for when applying styles breaks
         }
 
         if (this.options.source == 'auto') {
