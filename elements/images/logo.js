0a8aaf5405b1ea11e462d694a6744575a0fd3419
Luke-technolab <luke@technolableiden.nl>
2025-06-27 14:40:49 +0200

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -25,86 +25,91 @@ function deTranslate(canvas, dynamicImage, context) {
 async function generateDynamicIcon(image) {
     console.log(image)
 
+
     localforage.getItem(String(image)).then((value) => {
         // This code runs once the value has been loaded
         // from the offline store.
         console.log(value);
-    }).catch(async (err) => {
-        // This code runs if there were any errors
-        console.log(err);
+        if (value) {
 
-        await varExists('--primary');
+            // This code runs if there were any errors
+            console.log(err);
 
-        return new Promise((resolve) => {
-            //create 2 canvases 1 for reading and 1 for rendering the icon properly
-            const canvas = document.createElement('canvas');
-            const context = canvas.getContext("2d", { willReadFrequently: true });
+            await varExists('--primary');
 
-            const canvas2 = document.createElement('canvas');
-            const context2 = canvas2.getContext("2d", { willReadFrequently: true });
+            return new Promise((resolve) => {
+                //create 2 canvases 1 for reading and 1 for rendering the icon properly
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext("2d", { willReadFrequently: true });
+
+                const canvas2 = document.createElement('canvas');
+                const context2 = canvas2.getContext("2d", { willReadFrequently: true });
 
 
-            // Load your image onto the canvas
-            let dynamicImage = new Image();
-            dynamicImage.crossOrigin = 'anonymous';
+                // Load your image onto the canvas
+                let dynamicImage = new Image();
+                dynamicImage.crossOrigin = 'anonymous';
 
-            dynamicImage.src = image;
+                dynamicImage.src = image;
 
-            dynamicImage.onload = function () {
-                const root = document.documentElement;
+                dynamicImage.onload = function () {
+                    const root = document.documentElement;
 
-                rgb = getComputedStyle(root).getPropertyValue('--primary');
+                    rgb = getComputedStyle(root).getPropertyValue('--primary');
 
-                rgb = rgb.substring(5, rgb.length - 1);
-                rgb = rgb.split(',');
+                    rgb = rgb.substring(5, rgb.length - 1);
+                    rgb = rgb.split(',');
 
-                setTranslate(canvas, dynamicImage, context);
-                context.drawImage(dynamicImage, -dynamicImage.width / 2, -dynamicImage.height / 2);
+                    setTranslate(canvas, dynamicImage, context);
+                    context.drawImage(dynamicImage, -dynamicImage.width / 2, -dynamicImage.height / 2);
 
-                setTranslate(canvas2, dynamicImage, context2);
+                    setTranslate(canvas2, dynamicImage, context2);
 
-                // Get the entire image data as an array of pixel data
-                let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-                let textColor = getComputedStyle(root).getPropertyValue('--primaryTextColor') == 'rgb(255, 255, 255)' ? 'black' : 'white';
+                    // Get the entire image data as an array of pixel data
+                    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+                    let textColor = getComputedStyle(root).getPropertyValue('--primaryTextColor') == 'rgb(255, 255, 255)' ? 'black' : 'white';
 
-                let colorClass;
+                    let colorClass;
 
-                deTranslate(canvas, dynamicImage, context);
-                deTranslate(canvas2, dynamicImage, context2);
+                    deTranslate(canvas, dynamicImage, context);
+                    deTranslate(canvas2, dynamicImage, context2);
 
-                let x, y;
+                    let x, y;
 
-                for (y = 0; y < canvas.height; y++) {
-                    for (x = 0; x < canvas.width; x++) {
-                        const index = (y * canvas.width + x) * 4;
-                        const red = imageData.data[index];
-                        const green = imageData.data[index + 1];
-                        const blue = imageData.data[index + 2];
+                    for (y = 0; y < canvas.height; y++) {
+                        for (x = 0; x < canvas.width; x++) {
+                            const index = (y * canvas.width + x) * 4;
+                            const red = imageData.data[index];
+                            const green = imageData.data[index + 1];
+                            const blue = imageData.data[index + 2];
 
-                        //context.fillStyle = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`; // Set your desired color here
-                        //context.fillRect(x, y, 1, 1); // Draw a 5x5 square
+                            //context.fillStyle = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`; // Set your desired color here
+                            //context.fillRect(x, y, 1, 1); // Draw a 5x5 square
 
-                        if (colorClass != findColorClass(red, green, blue)) {
-                            // Replace the pixel with a 5x5 square
-                            context2.fillStyle = textColor; // Set your desired color here
-                            context2.fillRect(x, y, 2.5, 2.5); // Draw a 5x5 square
+                            if (colorClass != findColorClass(red, green, blue)) {
+                                // Replace the pixel with a 5x5 square
+                                context2.fillStyle = textColor; // Set your desired color here
+                                context2.fillRect(x, y, 2.5, 2.5); // Draw a 5x5 square
 
-                            colorClass = findColorClass(red, green, blue);
-                        }// else {
-                        //   x+=2; // skip a few pixels to increase loading speed
-                        //}
+                                colorClass = findColorClass(red, green, blue);
+                            }// else {
+                            //   x+=2; // skip a few pixels to increase loading speed
+                            //}
+                        }
                     }
-                }
 
-                context.clearRect(-2, -2, canvas.width + 5, canvas.height + 5);
+                    context.clearRect(-2, -2, canvas.width + 5, canvas.height + 5);
 
-                context.drawImage(canvas2, 0, 0);
+                    context.drawImage(canvas2, 0, 0);
 
-                window.localforage.setItem(String(image), canvas.toDataURL());
-                resolve(canvas.toDataURL());
-            };
-        });
-    });
+                    window.localforage.setItem(String(image), canvas.toDataURL());
+                    resolve(canvas.toDataURL());
+                };
+            });
+        } else {
+            resolve(value);
+        }
+    }).catch(async (err) => { });
 }
 
 async function setDynamicIcon(img, faviconUrl, radius) {
