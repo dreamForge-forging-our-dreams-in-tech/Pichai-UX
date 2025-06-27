3eec1059b0466c24f407c062e67fc5c2cf84caa5
Luke-technolab <luke@technolableiden.nl>
2025-06-27 13:26:03 +0200

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -72,12 +72,14 @@ async function generateDynamicIcon(image) {
                     //context.fillStyle = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`; // Set your desired color here
                     //context.fillRect(x, y, 1, 1); // Draw a 5x5 square
 
-                    if (colorClass != findColorClass(red, green, blue)) {
+                    let colorClass = findColorClass(red, green, blue);
+
+                    if (colorClass != colorClass) {
                         // Replace the pixel with a 5x5 square
                         context2.fillStyle = textColor; // Set your desired color here
                         context2.fillRect(x, y, 2.5, 2.5); // Draw a 5x5 square
 
-                        colorClass = findColorClass(red, green, blue);
+                        colorClass = colorClass;
                     }
                 }
             }
