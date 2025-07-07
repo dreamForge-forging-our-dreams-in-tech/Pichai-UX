0a5c28008338a675fbc1fe310b70160d45fa05ac
Luke-technolab <luke@technolableiden.nl>
2025-07-07 15:31:16 +0200

    test

--- AI/palleteMaker.js
+++ AI/palleteMaker.js
@@ -314,7 +314,7 @@ async function generate3ColorPallete(options) {
             // You can iterate through them and do something, e.g., add a border for visibility
             for (i of allElements) {
                 if (elementsWithTransparentBg.includes(i) || i == document.documentElement) { } else {
-                    i.style.backdropFilter = `blur(${options.blur}px)`; //add the blur effect
+                    i.style.backdropFilter = `blur(${parseInt(options.blur)}px)`; //add the blur effect
                     //console.log(i)
                 }
             }
