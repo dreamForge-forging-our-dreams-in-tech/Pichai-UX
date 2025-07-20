8d2cb79f9130936692b97f72b5a6947414af0eeb
dreamForge <54947833+lukeplays33@users.noreply.github.com>
2025-07-20 17:59:56 +0000

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -114,14 +114,14 @@ async function setDynamicIcon(img, faviconUrl) {
     console.log(faviconUrl)
 
     if(renderedIcons.hasownProperty(faviconUrl)) {
-        img.style.backgroundImage = renderedIcons[faviconUrl];
+        img.style.backgroundImage = renderedIcons[faviconUrl.style.backgroundImage];
         return;
     }
-    
+
     let newIcon = await generateDynamicIcon(faviconUrl);
     img.style.backgroundImage = `url(${newIcon})`;
 
-    renderedIcons[faviconUrl] = img;
+    renderedIcons[faviconUrl] = new;
     console.log(renderedIcons)
 }
 
