f35671d965170a8f2a44703ef14ebca5761fb706
dreamForge <54947833+lukeplays33@users.noreply.github.com>
2025-07-20 18:03:17 +0000

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -12,8 +12,6 @@ let rgb;
 let pixelSize = 6; //6; size of the pixel squares that are drawn on the canvas
 let size = 224; //224; // size of the canvas or image, this should be a square image
 
-let renderedIcons = {};
-
 function setTranslate(canvas) {
     canvas.width = size;
     canvas.height = size;
