dfb406bf3cc4bc9ecc268b27b63689ad8228fd9c
Luke-technolab <luke@technolableiden.nl>
2025-07-20 21:25:53 +0200

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -8,8 +8,8 @@ import '../../utils/localFOrage.js';
 
 let rgb;
 
-let pixelSize = 2.4; //2.4; size of the pixel squares that are drawn on the canvas
-let size = 80; //80; // size of the canvas or image, this should be a square image
+let pixelSize = 2.5; //2.4; size of the pixel squares that are drawn on the canvas
+let size = 100; //80; // size of the canvas or image, this should be a square image
 
 function setTranslate(canvas) {
     canvas.width = size;
