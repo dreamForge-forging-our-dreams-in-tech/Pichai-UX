cf2d73309ead866b13b1e9ebc92de8d1d5c61dba
dreamForge <54947833+lukeplays33@users.noreply.github.com>
2025-07-20 17:48:28 +0000

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -9,8 +9,8 @@ import '../../utils/localFOrage.js';
 
 let rgb;
 
-let pixelSize = 5; //6; size of the pixel squares that are drawn on the canvas
-let size = 200; //224; // size of the canvas or image, this should be a square image
+let pixelSize = 6; //6; size of the pixel squares that are drawn on the canvas
+let size = 224; //224; // size of the canvas or image, this should be a square image
 
 function setTranslate(canvas) {
     canvas.width = size;
