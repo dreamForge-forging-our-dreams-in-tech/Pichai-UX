500594b1e9791bdb80cc1238feda174c87f9aef7
dreamForge <54947833+lukeplays33@users.noreply.github.com>
2025-07-20 17:19:02 +0000

    test

--- elements/images/logo.js
+++ elements/images/logo.js
@@ -98,7 +98,7 @@ async function generateDynamicIcon(image) {
 
                         colorClass = findColorClass(red, green, blue);
                     } else {
-                        x += pixelSize - 5;
+                        x += pixelSize;
                     }
                 }
             }
