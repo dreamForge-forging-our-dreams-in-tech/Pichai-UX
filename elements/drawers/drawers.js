e4f7188839fa8c0774ff58d3bc4cbc094b31e65c
dreamForge <54947833+lukeplays33@users.noreply.github.com>
2025-08-20 10:11:00 +0200

    test

--- elements/drawers/drawers.js
+++ elements/drawers/drawers.js
@@ -18,6 +18,7 @@ function createSimpleDrawer(element, mode, open = true) { // turns a simple elem
         element.toggle.click();
     }
 
+    console.log(this, element)
     for (i of element.children) { // click event somehow handles mobile mode aswel
         i.addEventListener('click', getOrientation(this));
         screen.orientation.addEventListener("change", getOrientation(this));
